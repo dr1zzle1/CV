40c23063189ff02a75320e43f33bbbde36fe3c21
Marat Valiev <marat.rakhimov.2016@gmail.com>
2024-02-21 00:30:58 +0300

    bootstrap -> vanila css

--- js/script.js
+++ js/script.js
@@ -1,27 +1,19 @@
-// declare variable
-var scrollTop = $(".scrollTop");
+let scrollTop = document.querySelector(".scrollTop");
 
-$(window).scroll(function () {
-  // declare variable
-  var topPos = $(this).scrollTop();
+window.scroll(function () {
+  // declare letiable
+  let topPos = document.querySelector(this).scrollTop();
 
   // if user scrolls down - show scroll to top button
   if (topPos > 100) {
-    $(scrollTop).css("opacity", "1");
+    document.querySelector(scrollTop).css("opacity", "1");
   } else {
-    $(scrollTop).css("opacity", "0");
+    document.querySelector(scrollTop).css("opacity", "0");
   }
-}); // scroll END
-//Click event to scroll to top
-$(scrollTop).on("click", function () {
-  /*$('html, body').animate({
-      scrollTop: 0
-  }, 800);*/
-  alert("asdfj");
 });
 
-var app = document.getElementById("typeabletext");
-var typewriter = new Typewriter(app, {
+let app = document.getElementById("typeabletext");
+let typewriter = new Typewriter(app, {
   loop: true,
   delay: 75,
 });
@@ -44,10 +36,6 @@ setTimeout(() => {
   }
 }, 5000);
 
-function closeIntro() {
-  $("#intro").hide(1000);
-  $(".change_mod").css("position", "absolute");
-}
 function sendMail(ev) {
   ev.preventDefault();
   const name = document.getElementById("name").value;
@@ -60,22 +48,23 @@ function sendMail(ev) {
     }`
   );
 }
+
 const theme_mode = document.getElementById("theme-mode");
 const moon = document.getElementById("moon");
 const sun = document.getElementById("sun");
-var curr_mode = "dark";
+let curr_mode = "dark";
 function changeTheme() {
   if (curr_mode === "light") {
     moon.style.display = "none";
     sun.style.display = "block";
     curr_mode = "dark";
-    var element = document.body;
+    let element = document.body;
     element.classList.toggle("dark-mode");
   } else {
     sun.style.display = "none";
     moon.style.display = "block";
     curr_mode = "light";
-    var element = document.body;
+    let element = document.body;
     element.classList.toggle("dark-mode");
   }
 }
